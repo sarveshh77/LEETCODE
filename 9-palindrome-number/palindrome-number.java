74857a2cacc76f96c72449d051866f56ad3490c9
Sarvesh Warule <158740395+sarveshh77@users.noreply.github.com>
2025-06-13 23:31:11 +0530

    Time: 5 ms (69.27%) | Memory: 43.9 MB (76.38%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        
+        int temp=x;
+        int lastDigit;
+        int rev=0;
+
+        while(x>0)
+        {
+            lastDigit=x%10;
+            rev=(rev*10)+lastDigit;
+            x=x/10;
+        }
+        if(rev==temp)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+//Sarvesh Link
