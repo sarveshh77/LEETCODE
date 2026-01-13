4d24fe1d0b38efb32b96c5102b8ebe5611ae435c
Sarvesh Warule <158740395+sarveshh77@users.noreply.github.com>
2026-01-14 02:04:39 +0530

    Time: 4 ms (79.17%) | Memory: 46.8 MB (11.00%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) 
+    {
+        char arr1[]= s.toCharArray();
+        char arr2[]= t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(Arrays.equals(arr2,arr1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
