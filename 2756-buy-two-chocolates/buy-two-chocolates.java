9c76e782e1b5304cbf383aa8205c0cfd834f7087
Sarvesh Warule <158740395+sarveshh77@users.noreply.github.com>
2026-02-11 02:01:48 +0530

    Time: 3 ms (60.83%) | Memory: 46.5 MB (62.60%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,19 +1,33 @@
+// class Solution {
+//     public int buyChoco(int[] prices, int money) 
+//     {
+//         Arrays.sort(prices);
+
+//         if(prices[0]+prices[1]==money)
+//         {
+//             return 0;
+//         }
+//         else if(prices[0]+prices[1]>money)
+//         {
+//             return money;
+//         }
+//         else
+//         {
+//             return money-(prices[0]+prices[1]);
+//         }
+//     }
+// }
+
 class Solution {
-    public int buyChoco(int[] prices, int money) 
-    {
+    public int buyChoco(int[] prices, int money) {
         Arrays.sort(prices);
 
-        if(prices[0]+prices[1]==money)
-        {
-            return 0;
-        }
-        else if(prices[0]+prices[1]>money)
-        {
+        int sum = prices[0] + prices[1];
+
+        if (sum > money) {
             return money;
         }
-        else
-        {
-            return money-(prices[0]+prices[1]);
-        }
+
+        return money - sum;
     }
 }
